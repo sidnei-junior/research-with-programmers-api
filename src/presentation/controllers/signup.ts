1a5d8126d8d44c76e8aff784220d9bfde74c2fea
sidneiarauj <sidnei.ifrn@gmail.com>
2022-09-18 11:33:05 -0300

    feat: ensure SignUpController return 400 if no password is provider

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -3,12 +3,13 @@ import { HttpRequest, HttpResponse } from '../protocols/http'
 import { badRequest } from '../helpers/http-helper'
 
 export class SignUpController {
-  handle(httpRequest: HttpRequest): HttpResponse | undefined {
-    const requiredFields = ['name', 'email']
+  handle(httpRequest: HttpRequest): HttpResponse {
+    const requiredFields = ['name', 'email', 'password']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
       }
     }
+    return badRequest(new MissingParamError(''))
   }
 }
