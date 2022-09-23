3c7b79ca7466cca4c841455bf612c37d1cf81140
sidneiarauj <sidnei.ifrn@gmail.com>
2022-09-23 05:39:40 -0300

    chore: adjust jest configs and scripts

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,13 @@
 export default {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/protocols/*.ts',
+    '!<rootDir>/src/**/*protocols.ts',
+    '!<rootDir>/src/**/models/*.ts',
+    '!<rootDir>/src/**/usecases/*.ts',
+    '!<rootDir>/src/**/index.ts'
+  ],
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
