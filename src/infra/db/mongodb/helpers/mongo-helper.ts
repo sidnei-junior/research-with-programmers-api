82c1da5f455e783afee1b0687bbd34227e4b969a
sidneiarauj <sidnei.ifrn@gmail.com>
2022-10-12 14:36:35 -0300

    refactor: move mapping to MongoHelper

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -11,5 +11,10 @@ export const MongoHelper = {
   },
   getCollection(name: string): Collection {
     return this.client.db().collection(name)
+  },
+  map: (collection: any): any => {
+    const { _id, ...collectionWithoutId } = collection
+    const collectionMapped = Object.assign({}, collectionWithoutId, { id: _id.toHexString() })
+    return collectionMapped
   }
 }
