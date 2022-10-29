c8c9423357706659279861a325dcbaec58f60a70
sidneiarauj <sidnei.ifrn@gmail.com>
2022-10-29 05:58:29 -0300

    chore: add dotenv

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,9 +1,15 @@
 import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
 import env from './config/env'
 
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
 void MongoHelper.connect(env.mongoUrl)
   .then(async () => {
     const app = (await import('./config/app')).default
+    console.log('Banco: ', env.mongoUrl)
+    console.log('process.env.MONGO_URL: ', process.env.MONGO_URL)
     app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
   })
   .catch(console.error)
