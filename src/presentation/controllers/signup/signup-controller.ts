8bbbea70790905d8a191bcf27b2f93c55d3366bc
sidneiarauj <sidnei.ifrn@gmail.com>
2022-11-10 05:08:42 -0300

    feat: ensure SignUpController returns 403 if AddAccount returns null

--- src/presentation/controllers/signup/signup-controller.ts
+++ src/presentation/controllers/signup/signup-controller.ts
@@ -1,4 +1,5 @@
-import { badRequest, ok, serverError } from '../../helpers/http/http-helper'
+import { EmailInUseError } from '../../errors'
+import { badRequest, forbidden, ok, serverError } from '../../helpers/http/http-helper'
 import {
   Controller,
   HttpRequest,
@@ -22,11 +23,14 @@ export class SignUpController implements Controller {
         return badRequest(error)
       }
       const { name, password, email } = httpRequest.body
-      await this.addAccount.add({
+      const account = await this.addAccount.add({
         name,
         email,
         password
       })
+      if (!account) {
+        return forbidden(new EmailInUseError())
+      }
       const accessToken = await this.authentication.auth({
         email,
         password
